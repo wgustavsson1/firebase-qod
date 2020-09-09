e7ef3af3ab107e118893bcc5dcc622c5b734406b
Wilhelm Gustavsson <wilgu921@student.liu.se>
2020-09-09 03:58:28 +0200

    Changed to SPA

---
+++ public/footerMenu.js
@@ -0,0 +1,24 @@
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+// this is your `load_home() function`
+async function loadPage(page) {
+    const contentDiv = document.getElementById("page");
+    contentDiv.innerHTML = await fetchHtmlAsText(page);
+}
+
+document.getElementById("footer_menu_profile").onclick = function()
+{
+    loadPage("profile.html").then(function(){
+        setupPage();
+    });
+};
+
+document.getElementById("footer_menu_friends").onclick = function()
+{
+    loadPage("friends.html").then(function(){
+        setupPage();
+    });
+};
+
